1acfbfe6cacabd624901cc0aa2db926db9b47abd
Taylor Hernandez <95730939+TayHern@users.noreply.github.com>
2022-04-07 12:31:46 -0700

    add tests prompts

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,12 @@
+const Employee = require('../lib/Employee')
+
+console.log(newEmployee());
+
+test('create an employee object', () => {
+    const employee = new Employee('Taylor Hernandez', 2, 'test@test.com');
+
+    expect(employee.name).to.equal(expect.any(String));
+    expect(employee.id).to.equal(expect.any(Number));
+    expect(employee.email).to.equal(expect.any(String));
+
+});
